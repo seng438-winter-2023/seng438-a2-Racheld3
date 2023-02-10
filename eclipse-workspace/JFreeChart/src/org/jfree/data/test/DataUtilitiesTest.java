ca26a3655e7625ac337ffbbcea5c5c72e0f8460e
Racheld3 <47041999+Racheld3@users.noreply.github.com>
2023-02-10 09:25:47 -0700

    row and column tests

--- eclipse-workspace/JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ eclipse-workspace/JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -82,6 +82,44 @@ public class DataUtilitiesTest extends DataUtilities {
 	    // tear-down: NONE in this test method
 	}
 	
+	@Test
+	public void calculateColumnTotalForNoValues()
+	{
+	    // setup
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	        {
+	            one(values).getRowCount();
+	            will(returnValue(0));    
+	        }
+	    });
+	    double result = DataUtilities.calculateColumnTotal(values, 0);
+	    // verify
+	    assertEquals(0, result, .000000001d);
+	    // tear-down: NONE in this test method
+	}
+	
+	@Test
+	public void calculateColumnTotalForInvalidColumn()
+	{
+	    // setup
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	        {
+	            one(values).getRowCount();
+	            will(returnValue(1)); 
+	            one(values).getValue(0, -1);
+	            will(returnValue(0));
+	        }
+	    });
+	    double result = DataUtilities.calculateColumnTotal(values, -1);
+	    // verify
+	    assertEquals(0, result, .000000001d);
+	    // tear-down: NONE in this test method
+	}
+	
 	//calculateRowTotal(Values2D, int)
 	
 	@Test
@@ -147,6 +185,43 @@ public class DataUtilitiesTest extends DataUtilities {
 	    // tear-down: NONE in this test method
 	}
 	
+	@Test
+	public void calculateRowTotalForNoValues()
+	{
+	    // setup
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	        {
+	            one(values).getColumnCount();
+	            will(returnValue(0));    
+	        }
+	    });
+	    double result = DataUtilities.calculateRowTotal(values, 0);
+	    // verify
+	    assertEquals(0, result, .000000001d);
+	    // tear-down: NONE in this test method
+	}
+	
+	@Test
+	public void calculateRowTotalForInvalidColumn()
+	{
+	    // setup
+	    Mockery mockingContext = new Mockery();
+	    final Values2D values = mockingContext.mock(Values2D.class);
+	    mockingContext.checking(new Expectations() {
+	        {
+	            one(values).getColumnCount();
+	            will(returnValue(1)); 
+	            one(values).getValue(-1, 0);
+	            will(returnValue(0));
+	        }
+	    });
+	    double result = DataUtilities.calculateRowTotal(values, -1);
+	    // verify
+	    assertEquals(0, result, .000000001d);
+	    // tear-down: NONE in this test method
+	}
 	
 	//CreateNumberArray(double[]) tests
 	
@@ -179,18 +254,7 @@ public class DataUtilitiesTest extends DataUtilities {
 		
 		// tear-down: NONE in this test method
 	}
-	
-	@Test
-	public void createNumberArrayForNullArray() {
-		//setup
-		double[] testArray = null;
-		
-		//test
-		//verify
-		//assertThrows(DataUtilites.class, () -> {DataUtilities.createNumberArray(testArray); });
-		
-		// tear-down: NONE in this test method
-	}
+
 	
 	@Test(expected = InvalidParameterException.class)
 	public void testCreateNumberArray2D(){
