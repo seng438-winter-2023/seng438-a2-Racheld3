ce89c12f66d2364c0ebacfce6276501ac8ab9503
Racheld3 <47041999+Racheld3@users.noreply.github.com>
2023-02-09 17:13:26 -0700

    get lower bound test

--- eclipse-workspace/JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ eclipse-workspace/JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -15,51 +15,10 @@ public class RangeTest{
 	@Test
 	public void getLowerBoundForValidRange() 
 	{
-		
 		//setup
-		Mockery mockingContext = new Mockery() {{
-	        setImposteriser(ClassImposteriser.INSTANCE);
-	    }};
-	    
-	    final Range testRange = mockingContext.mock(org.jfree.data.Range.class);
-	    mockingContext.checking(new Expectations() {
-	        {
-	            one(testRange).getLowerBound();
-	            will(returnValue(2));
-	            one(testRange).getUpperBound();
-	            will(returnValue(3));
-	        }
-	    });
-	    
+		final Range testRange = new Range(2, 3);
 		double result = testRange.getLowerBound();
 		double expectedResult = 2;
-		
-		//verify
-		assertEquals(result, expectedResult, 0.00001); //dunno how precise it needs to be
-		
-	}
-	
-	@Test
-	public void getLowerBoundForInvalidRange() 
-	{
-		
-		//setup
-		Mockery mockingContext = new Mockery() {{
-	        setImposteriser(ClassImposteriser.INSTANCE);
-	    }};
-	    
-	    final Range testRange = mockingContext.mock(org.jfree.data.Range.class);
-	    mockingContext.checking(new Expectations() {
-	        {
-	            one(testRange).getLowerBound();
-	            will(returnValue(3));
-	            one(testRange).getUpperBound();
-	            will(returnValue(2));
-	        }
-	    });
-	    
-		double result = testRange.getLowerBound();
-		double expectedResult = 2; //is this the expected???
 		
 		//verify
 		assertEquals(result, expectedResult, 0.00001); //dunno how precise it needs to be
